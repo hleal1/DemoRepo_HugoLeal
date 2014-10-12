6e811365948047276acf9320a30c3ecf0d28fd86
hleal <hugoleal@local.com>
2014-10-12 23:55:25 +0100

    add SumNumbers exercise

---
+++ SumNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class SumNumbers {
+
+		public static void main(String[] args) 
+		{
+			Scanner kb = new Scanner(System.in);
+			System.out.println ("Introduza dois números");
+			int numbers[] = new int[2];
+	
+			int num1 = numbers[0];
+			int num2 = numbers[1];
+			System.out.println(num1 + "+" + num2 + "=" + sum(num1, num2));
+			}
+		
+		//método para a soma dos dois números
+		private static int sum(int num1, int num2) {
+		
+			return num1 + num2;
+
+	}
+	
+
+}
